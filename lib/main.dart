3dd8d2fc32cb324bcceaa0c5c0a9649fea1ee80e
dudnjsckrgo <dudnjsckrgo@gmail.com>
2024-12-05 12:53:04 +0900

    usePathUrlStrategy

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -12,11 +13,10 @@ import 'config/app_config.dart';
 import 'providers/auth_provider.dart';
 
 // 웹 전용 import를 조건부로 처리
-import 'web_url_strategy.dart' if (dart.library.html) 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  usePathUrlStrategy();
   const env = String.fromEnvironment('ENV', defaultValue: 'dev');
   AppConfig.setEnvironment(env == 'prod' ? Environment.prod : Environment.dev);
 
